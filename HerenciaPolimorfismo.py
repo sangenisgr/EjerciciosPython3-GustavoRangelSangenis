f153e891e947a8accacaa2ce369404d0cd5cfdda
sangenisgr <sangenisgr@gmail.com>
2020-06-16 16:25:35 -0500

    Ejercicio Herencia y Polimorfismo

---
+++ HerenciaPolimorfismo.py
@@ -0,0 +1,14 @@
+#Creamos nuestra clase "Figura"
+class Figura:
+    def __init__(self,lado,altura,base,radio):
+        self.lado = lado
+        self.altura = altura
+        self.base = base
+        self.radio = radio
+
+#Heredamos las clases RECTANGULO y TRIANGULO de la clase FIGURA
+class Rectangulo(Figura):
+
+class triangulo(Figura):
+
+
